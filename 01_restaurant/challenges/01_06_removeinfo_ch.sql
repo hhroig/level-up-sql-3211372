133c3cb43a33d8a11540ddf5f1cb051de7187a04
Harold A. Hdez <34509768+hhroig@users.noreply.github.com>
2023-08-02 12:00:06 +0000

    sols 01_05-06

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -1,5 +1,25 @@
 -- Remove incorrect information from the database.
 
 -- A customer named Norby has notified us he won't 
--- be able to keep his Friday reservation. 
--- Today is July 24, 2022.
+-- be able to keep his Friday reservation.  (July 29, 2022)
+-- Today is July 24, 2022. 
+
+SELECT * FROM Customers
+WHERE FirstName = 'Norby';
+
+-- Check which one is it:
+SELECT * FROM Reservations
+WHERE CustomerID = ( SELECT CustomerID FROM Customers
+WHERE FirstName = 'Norby' ) AND Date > '2022-07-24';
+
+
+-- Remove:
+DELETE FROM Reservations
+WHERE CustomerID = ( SELECT CustomerID FROM Customers
+WHERE FirstName = 'Norby' ) AND Date > '2022-07-24';
+
+-- Check:
+SELECT * FROM Reservations
+WHERE CustomerID = ( SELECT CustomerID FROM Customers
+WHERE FirstName = 'Norby' )
+ORDER BY Date DESC;
