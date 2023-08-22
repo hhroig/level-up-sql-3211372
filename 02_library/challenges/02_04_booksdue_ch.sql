dcfed35b180281fc8cc9666217731cffa7a33c7b
Harold A. Hdez <34509768+hhroig@users.noreply.github.com>
2023-08-22 18:35:03 +0000

    sol II: 04

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,16 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+
+SELECT *
+FROM Loans
+LEFT JOIN Patrons ON Patrons.PatronID = Loans.PatronID
+LEFT JOIN Books ON Books.BookID = Loans.BookID
+WHERE DueDate = '2022-07-13';
+
+
+SELECT DueDate, Title, FirstName, Email 
+FROM Loans
+LEFT JOIN Patrons ON Patrons.PatronID = Loans.PatronID
+LEFT JOIN Books ON Books.BookID = Loans.BookID
+WHERE DueDate = '2022-07-13';
