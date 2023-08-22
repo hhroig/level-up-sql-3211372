556bc1b214041b33773de0647aefedccddfb4890
Harold A. Hdez <34509768+hhroig@users.noreply.github.com>
2023-08-22 18:22:49 +0000

    sols II: 01-03

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,11 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+
+SELECT COUNT(*) AS CountDracula FROM Books
+WHERE Title = 'Dracula';
+
+SELECT COUNT(*) AS CountDraculaLoans FROM Loans
+WHERE BookID in (SELECT BookID FROM Books WHERE Title = 'Dracula') 
+AND ReturnedDate ISNULL;
+
+-- There's 1 in loan and 2 available!
