5a326885c70988fd7b0b43651c3b1fb0bc0646f7
Harold A. Hdez <34509768+hhroig@users.noreply.github.com>
2023-08-22 14:55:48 +0000

    sol ex7

--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -2,3 +2,12 @@
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+
+SELECT * FROM CostumersParty;
+
+SELECT CustomerID FROM Customers
+WHERE Email = 'atapley2j@kinetecoinc.com';
+
+INSERT INTO CostumersParty(CustomerID, PartySize)
+VALUES((SELECT CustomerID FROM Customers
+WHERE Email = 'atapley2j@kinetecoinc.com'), 3);
